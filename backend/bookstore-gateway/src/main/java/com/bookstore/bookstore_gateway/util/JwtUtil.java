40613025f2bc7b23092be545627e7e4e299367a5
Adnan-Haque <79236923+Adnan-Haque@users.noreply.github.com>
2024-07-03 19:43:52 +0530

    Update JwtUtil.java

--- backend/bookstore-gateway/src/main/java/com/bookstore/bookstore_gateway/util/JwtUtil.java
+++ backend/bookstore-gateway/src/main/java/com/bookstore/bookstore_gateway/util/JwtUtil.java
@@ -3,6 +3,7 @@ package com.bookstore.bookstore_gateway.util;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -11,7 +12,8 @@ import java.security.Key;
 public class JwtUtil {
 
 
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    @Value("${SECRET_KEY}")
+    public static String SECRET;
 
 
     public void validateToken(final String token) {
