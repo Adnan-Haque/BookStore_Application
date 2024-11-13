1c28964ad395cb36b8112a9de2b7798e7964b5fd
Adnan-Haque <79236923+Adnan-Haque@users.noreply.github.com>
2024-11-13 15:31:22 +0530

    Cookies Added

--- Backend/gateway/src/main/java/com/bookstore/gateway/controller/GatewayController.java
+++ Backend/gateway/src/main/java/com/bookstore/gateway/controller/GatewayController.java
@@ -5,12 +5,14 @@ import com.bookstore.gateway.dto.CredentialsDTO;
 import com.bookstore.gateway.dto.TokenResponseDTO;
 import com.bookstore.gateway.service.KeycloakTokenService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -31,9 +33,28 @@ public class GatewayController {
     }
 
     @PostMapping("/login-user")
-    public Mono<ResponseEntity<Object>> login(@RequestBody Map<String, String> map) {
+    public Mono<ResponseEntity<Object>> login(@RequestBody Map<String, String> map, ServerWebExchange exchange) {
         return keycloakTokenService.getToken(map)
-                .map(tokenResponse -> ResponseEntity.ok((Object) tokenResponse))
+                .flatMap(tokenResponse -> {
+                    ResponseCookie accessTokenCookie = ResponseCookie.from("access_token", tokenResponse.getAccess_token())
+                            .httpOnly(true)
+                            .path("/")
+                            .maxAge(tokenResponse.getExpires_in()) // Set based on token life
+                            .build();
+
+                    // Create refresh token cookie
+                    ResponseCookie refreshTokenCookie = ResponseCookie.from("refresh_token", tokenResponse.getRefresh_token())
+                            .httpOnly(true)
+                            .path("/")
+                            .maxAge(tokenResponse.getRefresh_expires_in()) // Set longer for refresh token
+                            .build();
+
+                    // Add cookies to the response
+                    exchange.getResponse().addCookie(accessTokenCookie);
+                    exchange.getResponse().addCookie(refreshTokenCookie);
+
+                    return Mono.just(ResponseEntity.ok((Object) tokenResponse));
+                })
                 .onErrorResume(ResponseStatusException.class, ex -> {
                     return Mono.just(ResponseEntity.status(ex.getStatusCode()).body(ex.getReason()));
                 });
